8771318eb24f2a1f116eb093f1bd16e4bcba0f8a
Shubham_Saxena555 <shubham@outshade.net>
2020-11-10 10:57:37 +0530

    Gujrat

--- admin/moderators.php
+++ admin/moderators.php
@@ -203,7 +203,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Kuberan's House 2020</span>
+          <span>Copyright &copy; Startup India - Gujrat 2020</span>
           </div>
         </div>
       </footer>
